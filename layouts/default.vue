87c407332e7a20e875529087dfaf431e3317ec36
Maximo <maxmonteiro86@gmail.com>
2023-11-07 14:21:16 -0300

    cria layout base das paginas empresa, servicos e produtos

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+        <NuxtLink :to="{ name: 'a-empresa'}">A empresa</NuxtLink>
+        <NuxtLink :to="{ name: 'servicos'}">Serviços</NuxtLink>
+        <NuxtLink :to="{ name: 'produtos'}">Produtos</NuxtLink>
+      </div>
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
---
+++ pages/produtos/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1 class="text-lg font-bold">Produtos</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi optio recusandae officia, possimus corrupti velit aperiam consequuntur nesciunt quibusdam eaque quidem labore unde fugiat magni aliquid, numquam magnam? Assumenda, voluptatum.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
