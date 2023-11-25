caa9b5fcf62f366dc85f660c13ede30c67882f24
Maximo <maxmonteiro86@gmail.com>
2023-11-25 13:09:31 -0300

    exemplo busca de dados com asyncData e axios

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -3,6 +3,16 @@
     <div class="container mx-auto">
       <h1 class="text-lg font-bold">Produtos</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi optio recusandae officia, possimus corrupti velit aperiam consequuntur nesciunt quibusdam eaque quidem labore unde fugiat magni aliquid, numquam magnam? Assumenda, voluptatum.</p>
+
+    <br>
+    <br>
+
+    <div v-for="product in products" :key="product.id"
+      class="border-b border-gray-400 py-3"
+    >
+      {{  product.title }}
+    </div>
+
     </div>
   </div>
 </template>
@@ -17,13 +27,12 @@ export default {
     }
   },
 
-  async asyncData() {
-    await new Promise( (resolve) => {
-      setTimeout(() => {
-        resolve()
-        console.log('Promise resolved')
-      }, 3000);
-    })
+  async asyncData({ $axios }) {
+    const products = await $axios.$get('https://jsonplaceholder.typicode.com/posts?_limit=3')
+
+    return {
+      products
+    }
   },
 
   methods: {
